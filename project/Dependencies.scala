380e78b058415b05d2e30b3f84ae6c909a9ac83b
Scala Steward <me@scala-steward.org>
2020-02-24 17:17:28 +0100

    Update async-http-client-backend-zio, ... to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     "dev.zio"                      %% "zio-streams"                   % zioVersion,
     "dev.zio"                      %% "zio-interop-java"              % "1.1.0.0-RC6",
     "dev.zio"                      %% "zio-logging"                   % "0.0.4",
-    "com.softwaremill.sttp.client" %% "core"                          % "2.0.0-RC5",
-    "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % "2.0.0-RC5"
+    "com.softwaremill.sttp.client" %% "core"                          % "2.0.0",
+    "com.softwaremill.sttp.client" %% "async-http-client-backend-zio" % "2.0.0"
   )
 }
